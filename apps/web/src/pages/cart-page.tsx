3cd71c7eba8cdc5224b15b485859e07795339668
AnonyFriday <duykimvu75@gmail.com>
2026-08-06 14:05:11 +0700

    feat: add other routes

---
+++ apps/web/src/pages/cart-page.tsx
@@ -0,0 +1,18 @@
+export function CartPage() {
+  return (
+    <div>
+      <section className="relative grid min-h-79 place-items-center overflow-hidden">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="/images/common/common-10.jpg"
+          alt=""
+        />
+        <div className="absolute inset-0 bg-white/55 backdrop-blur-[1px]"></div>
+        <div className="relative text-center">
+          <h1 className="text-[48px] font-medium text-black">Cart</h1>
+          <p className="mt-2 text-base font-normal text-black">Home &gt; Cart</p>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ apps/web/src/pages/single-product-page.tsx
@@ -0,0 +1,7 @@
+export function SingleProductPage() {
+  return (
+    <div className="mx-auto w-[min(1240px,calc(100%-32px))] py-12">
+      <h1 className="text-3xl font-bold">Single Product Page</h1>
+    </div>
+  );
+}
